363732b1123e2c1356be3a8a17b98410d7ea2491
12691282 <12691282@qq.com>
2025-11-18 15:17:15 +0800

    modify

--- admin/admin-system/src/main/java/mgr/AppRun.java
+++ admin/admin-system/src/main/java/mgr/AppRun.java
@@ -28,8 +28,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author Zheng Jie
- * @date 2018/11/15 9:20:19
+ * @author fls
  */
 @Slf4j
 @RestController
